02734f54c0ceb72c620170ee674fbb9d75413f37
ddoss777 <looteraddos@gmail.com>
2024-12-24 15:10:02 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 from threading import Thread
 
-app = Flask(__name__)
+app = Flask(__root__)
 @app.route('/')
 def index():
     return "Alive"
 
 def run():
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host=194.62.97.191,port=8080)
 
 def keep_alive():
-    t = Thread(target=run)
+    t = Thread(shubhhhh)
     t.start()    
